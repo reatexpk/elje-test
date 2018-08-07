1a74c926e60954e23d1b8d8703fbe2f3e62b16e6
reatexpk <32232046+reatexpk@users.noreply.github.com>
2018-08-07 10:13:47 +0500

    Feat/nav (#2)
    * Created navigation block
    
    * Created cover image and titles
    
    * Created about block (#3)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import styled from 'styled-components';
 
 import './fonts/fonts.css';
 import Header from './Header/index';
+import Navigation from './Navigation/index';
+import CoverImage from './CoverImage/index';
+import About from './About/index';
 
 const StyledApp = styled.div`
   box-sizing: border-box;
@@ -12,6 +15,9 @@ const StyledApp = styled.div`
 const App = () => (
   <StyledApp>
     <Header />
+    <Navigation />
+    <CoverImage />
+    <About />
   </StyledApp>
 );
 
