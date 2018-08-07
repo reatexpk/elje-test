24b76b35462c131201291fcde1caa0b18d3ec89e
reatex <reatexpk1@gmail.com>
2018-08-07 09:52:04 +0500

    Created cover image and titles

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import './fonts/fonts.css';
 import Header from './Header/index';
 import Navigation from './Navigation/index';
+import CoverImage from './CoverImage/index';
 
 const StyledApp = styled.div`
   box-sizing: border-box;
@@ -14,6 +15,7 @@ const App = () => (
   <StyledApp>
     <Header />
     <Navigation />
+    <CoverImage />
   </StyledApp>
 );
 
