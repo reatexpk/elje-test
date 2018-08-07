04178361e550e1b085d41a2900d7bef9a0bea5c8
reatex <reatexpk1@gmail.com>
2018-08-07 08:15:18 +0500

    Created header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const App = () => <div className="App" />;
+import './fonts/fonts.css';
+import Header from './Header/index';
+
+const StyledApp = styled.div`
+  box-sizing: border-box;
+  font-family: Navigo;
+`;
+
+const App = () => (
+  <StyledApp>
+    <Header />
+  </StyledApp>
+);
 
 export default App;
