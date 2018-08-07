25b678934c4a1e5537273d2d5c36f4a5ec6f7d2a
reatex <reatexpk1@gmail.com>
2018-08-08 00:42:53 +0500

    Added flexboxgrid2 and container for header and nav

--- src/Navigation/index.js
+++ src/Navigation/index.js
@@ -6,21 +6,28 @@ import MapButton from './MapButton';
 import Menu from './Menu';
 import AccountButton from './AccountButton';
 
-const StyledNav = styled.nav`
+const NavWrapper = styled.nav`
+  background-color: #3d4252;
+`;
+
+const StyledNav = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 13px 0;
-  background-color: #3d4252;
 `;
 
 const Navigation = () => (
-  <StyledNav>
-    <SearchButton />
-    <MapButton />
-    <Menu />
-    <AccountButton />
-  </StyledNav>
+  <NavWrapper>
+    <div className="container">
+      <StyledNav>
+        <SearchButton />
+        <MapButton />
+        <Menu />
+        <AccountButton />
+      </StyledNav>
+    </div>
+  </NavWrapper>
 );
 
 export default Navigation;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'normalize.css';
+import 'flexboxgrid2';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
