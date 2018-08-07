55f34f2192cdf4a2a2165e3016dd474b5595b4fe
reatexpk <32232046+reatexpk@users.noreply.github.com>
2018-08-07 10:13:29 +0500

    Created about block (#3)

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './fonts/fonts.css';
 import Header from './Header/index';
 import Navigation from './Navigation/index';
 import CoverImage from './CoverImage/index';
+import About from './About/index';
 
 const StyledApp = styled.div`
   box-sizing: border-box;
@@ -16,6 +17,7 @@ const App = () => (
     <Header />
     <Navigation />
     <CoverImage />
+    <About />
   </StyledApp>
 );
 
