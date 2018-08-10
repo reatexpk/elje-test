339f596fac87ec1931f6ff959b19673e71bf67a8
reatex <reatexpk1@gmail.com>
2018-08-10 05:13:16 +0500

    Ended up with footer

--- src/Footer/Contacts.js
+++ src/Footer/Contacts.js
@@ -1,6 +1,59 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Contacts = () => <div />;
+import iconPhone from './icon_phone.svg';
+
+const ContactsWrapper = styled.div``;
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  margin: 0 auto;
+`;
+
+const Icon = styled.img`
+  margin: 3px 8px 0 48px;
+  background-color: #a8f0de;
+  width: 16px;
+  height: 16px;
+`;
+
+const Text = styled.p`
+  margin: 0;
+  color: #ffffff;
+  font-size: 18px;
+  font-weight: 400;
+  line-height: 26px;
+  text-align: center;
+`;
+
+const Button = styled.button`
+  width: 188px;
+  height: 34px;
+  margin: 20px 0 0 45px;
+  background-color: transparent;
+  border: 2px solid #ffffff;
+  border-radius: 19px;
+  color: #ffffff;
+  font-size: 12px;
+  font-weight: 700;
+  letter-spacing: 1px;
+  line-height: 16px;
+`;
+
+const Contacts = () => (
+  <ContactsWrapper>
+    <Container>
+      <Icon src={iconPhone} />
+      <Text>
++7 (495) 644 45 44
+      </Text>
+    </Container>
+    <Button>
+ОБРАТНАЯ СВЯЗЬ
+    </Button>
+  </ContactsWrapper>
+);
 
 export default Contacts;
--- src/Footer/Menu.js
+++ src/Footer/Menu.js
@@ -1,6 +1,56 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Menu = () => <div />;
+const StyledMenu = styled.ul`
+  margin: 0;
+  padding: 7px 45px 1px 45px;
+  color: #ffffff;
+  font-size: 12px;
+  font-weight: 700;
+  letter-spacing: 1px;
+  line-height: 22px;
+  list-style-type: none;
+  border-right: 1px solid rgba(255, 255, 255, 0.1);
+`;
+
+const Item = styled.li``;
+
+const Link = styled.a`
+  color: inherit;
+  text-decoration: none;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const Menu = () => (
+  <StyledMenu>
+    <Item>
+      <Link href="localhost">
+АРЕНДАТОРАМ
+      </Link>
+    </Item>
+    <Item>
+      <Link href="localhost">
+ЗАКУПКИ
+      </Link>
+    </Item>
+    <Item>
+      <Link href="localhost">
+ВАКАНСИИ
+      </Link>
+    </Item>
+    <Item>
+      <Link href="localhost">
+РЕКЛАМНЫЕ УСЛУГИ
+      </Link>
+    </Item>
+    <Item>
+      <Link href="localhost">
+КОНТАКТЫ
+      </Link>
+    </Item>
+  </StyledMenu>
+);
 
 export default Menu;
